ad2bbc63b892f76caacbfea9748d313cb9fffd3a
cesaroer <cesarvarcard@gmail.com>
2021-07-06 23:19:48 -0500

    Splash Screen design added

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,23 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "JobFinder",
       home: SplashScreen(),
+      theme: ThemeData(
+        primaryColor: Color(0XFF5F5FFF),
+        accentColor: Color(0xFF030047),
+        highlightColor: Color(0xFFB7B7D2),
+        textTheme: TextTheme(
+          headline1: TextStyle(
+            fontSize: 36.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF030047),
+          ),
+          headline2: TextStyle(
+            fontSize: 36.0,
+            fontWeight: FontWeight.bold,
+            color: Color(0xFF5F5FFF),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -4,7 +4,41 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        width: double.infinity,
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset("assets/imgs/img_main.png"),
+            Column(
+              children: [
+                Text(
+                  "Job hunting",
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                Text(
+                  "made aesy",
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+              ],
+            ),
+            MaterialButton(
+              onPressed: () {},
+              elevation: 10.0,
+              minWidth: 170.0,
+              height: 50.0,
+              color: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8.0)),
+              child: Text(
+                "Get Started",
+                style: TextStyle(color: Colors.white, fontSize: 20.0),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
